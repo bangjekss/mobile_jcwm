d58b004f925a4296c1b7a8e41e76c37cd4f38ffd
bangjekss <mrazak.main@mydrive.id>
2021-02-19 10:41:52 +0700

    day1

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text} from 'react-native';
+// import {configureStore} from '@reduxjs/toolkit';
+// import {Provider} from 'react-redux';
+// import reducer from './src/redux/reducers';
+// import {
+//   // Main,
+//   Todo,
+// } from './src/screens';
+// import {Header} from './src/components';
+
+// const store = configureStore({
+//   reducer,
+// });
+
+const App = () => {
+  return (
+    <Text>hi</Text>
+    // <Provider store={store}>
+    //   {/* <Header /> */}
+    //   <Todo />
+    // </Provider>
+  );
+};
+
+export default App;
