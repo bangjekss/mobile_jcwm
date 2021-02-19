8da149b396b80ba535202cd1d5992aa4a90c4095
bangjekss <mrazak.main@mydrive.id>
2021-02-19 20:04:38 +0700

    day2

--- App.js
+++ App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import {Text} from 'react-native';
-// import {configureStore} from '@reduxjs/toolkit';
-// import {Provider} from 'react-redux';
-// import reducer from './src/redux/reducers';
-// import {
-//   // Main,
-//   Todo,
-// } from './src/screens';
-// import {Header} from './src/components';
-
-// const store = configureStore({
-//   reducer,
-// });
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableNativeFeedbackComponent,
+  FlatList,
+} from 'react-native';
+import faker from 'faker';
 
 const App = () => {
+  const [data, setData] = useState([
+    {id: 1, nama: 'Bambang'},
+    {id: 2, nama: 'Joko'},
+    {id: 3, nama: 'Andi'},
+  ]);
   return (
-    <Text>hi</Text>
-    // <Provider store={store}>
-    //   {/* <Header /> */}
-    //   <Todo />
-    // </Provider>
-  );git 
+    <View style={styles.mainContainer}>
+      <FlatList
+        refreshing
+        // data={data}
+        data={Array.from(Array(50), () => faker.commerce.product())}
+        renderItem={({item}) => (
+          <View style={styles.productWrapper}>
+            <Text style={styles.productText}>{item}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'rgb(250, 202, 0)',
+  },
+  productText: {
+    fontSize: 25,
+  },
+  productWrapper: {
+    margin: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 50,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
